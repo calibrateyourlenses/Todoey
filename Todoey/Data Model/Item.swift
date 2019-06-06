b81e8e02743a0e962990ea48522a1392dc275e71
Lawrence Lim <initial_z@hotmail.com>
2019-06-06 00:29:52 -0600

    Convert to MVC design pattern. Create Item Class.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Lawrence on 6/5/19.
+//  Copyright © 2019 Lawrence Lim. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    //String title
+    //Bool done
+    var title: String = ""
+    var done: Bool = false
+    
+}
